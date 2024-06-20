18f66ea7c6a3934b2dc9c3a50ca919be954a9e2c
Karthik Soman <karthi.soman@gmail.com>
2024-06-20 00:33:11 -0700

    updated app by adding temperatur slider

--- app.py
+++ app.py
@@ -2,30 +2,33 @@ import streamlit as st
 import requests
 import time
 
-
-def get_response(user_input):
+def get_response(user_input, temperature):
     url = "https://jfn1so7p7a.execute-api.us-west-1.amazonaws.com/Prod/inference"
     headers = {
         "Content-Type": "application/json"
     }
     data = {
-        "text": user_input
+        "text": user_input,
+        "temperature": temperature
     }
     response = requests.post(url, headers=headers, json=data)
     return response.json()
 
-def response_generator(user_input):
-    response = get_response(user_input)
+def response_generator(user_input, temperature):
+    response = get_response(user_input, temperature)
     for word in response.split():
         yield word + " "
         time.sleep(0.05)
-
 
 st.title("Chat EDS with Zebra Llama")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+
+# Add a sidebar for the temperature slider
+with st.sidebar:
+    temperature = st.slider("Temperature", 0.0, 1.0, 0.7, 0.1)
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
@@ -36,6 +39,7 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("Chat about EDS"):
     # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
+
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
@@ -43,6 +47,7 @@ if prompt := st.chat_input("Chat about EDS"):
     # Display assistant response in chat message container
     with st.spinner("Wait for ~10-15 seconds. Processing... "):
         with st.chat_message("assistant"):
-            response = st.write_stream(response_generator(prompt))
+            response = st.write_stream(response_generator(prompt, temperature))
+
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
